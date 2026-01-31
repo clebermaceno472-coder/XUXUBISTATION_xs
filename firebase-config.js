681337d7821471fd0f8acc42057787844eccf911
clebermaceno472-coder <clebermaceno472@gmail.com>
2026-01-31 20:46:04 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,10 @@
-// firebase-config.js
-// Cole aqui o SEU firebaseConfig do painel do Firebase
-// (Configurações do projeto > Geral > App Web > firebaseConfig)
-
-window.XUXUNET_FIREBASE_CONFIG = {
-  apiKey: "COLE_AQUI",
-  authDomain: "COLE_AQUI",
-  projectId: "COLE_AQUI",
-  storageBucket: "COLE_AQUI",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI"
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCCeaZxNNgPMD22P62ZlvmIUcVWRYf9JAI",
+  authDomain: "xuxunet-1fefe.firebaseapp.com",
+  projectId: "xuxunet-1fefe",
+  storageBucket: "xuxunet-1fefe.firebasestorage.app",
+  messagingSenderId: "183630461451",
+  appId: "1:183630461451:web:1f388ccff35674de5f650e",
+  measurementId: "G-B92GR7N35E"
 };
